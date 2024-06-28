426a84f2acd4c7e4197af352a37d766a52331d91
YashasSingh <121969113+YashasSingh@users.noreply.github.com>
2024-06-28 15:24:05 -0400

    Add files via upload

--- tradingbot.py
+++ tradingbot.py
@@ -7,9 +7,6 @@ from alpaca_trade_api import REST
 from timedelta import Timedelta 
 from finbert_utils import estimate_sentiment
 
-API_KEY = "Y" 
-API_SECRET = "xxxx
-BASE_URL = "hs"
 
 ALPACA_CREDS = {
     "API_KEY":API_KEY, 
@@ -25,4 +22,30 @@ class MLTrader(Strategy):
         self.cash_at_risk = cash_at_risk
         self.api = REST(base_url=BASE_URL, key_id=API_KEY, secret_key=API_SECRET)
 
-  
+    def position_sizing(self): 
+        cash = self.get_cash() 
+        last_price = self.get_last_price(self.symbol)
+        quantity = round(cash * self.cash_at_risk / last_price,0)
+        return cash, last_price, quantity
+
+    def get_dates(self): 
+        today = self.get_datetime()
+        three_days_prior = today - Timedelta(days=3)
+        return today.strftime('%Y-%m-%d'), three_days_prior.strftime('%Y-%m-%d')
+
+    def get_sentiment(self): 
+        today, three_days_prior = self.get_dates()
+        news = self.api.get_news(symbol=self.symbol, 
+                                 start=three_days_prior, 
+                                 end=today) 
+        news = [ev.__dict__["_raw"]["headline"] for ev in news]
+        probability, sentiment = estimate_sentiment(news)
+        return probability, sentiment 
+
+    def on_trading_iteration(self):
+        cash, last_price, quantity = self.position_sizing() 
+        probability, sentiment = self.get_sentiment()
+
+        if cash > last_price: 
+            if sentiment == "positive" and probability > .999: 
+               
