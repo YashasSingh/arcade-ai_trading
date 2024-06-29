0696ec30bac2eb6a7fc11d10c7a7a403c0ef0523
YashasSingh <121969113+YashasSingh@users.noreply.github.com>
2024-06-28 23:02:22 -0400

    Add files via upload

--- tradingbot.py
+++ tradingbot.py
@@ -83,3 +83,12 @@ broker = Alpaca(ALPACA_CREDS)
 strategy = MLTrader(name='mlstrat', broker=broker, 
                     parameters={"symbol":"SPY", 
                                 "cash_at_risk":.5})
+strategy.backtest(
+    YahooDataBacktesting, 
+    start_date, 
+    end_date, 
+    parameters={"symbol":"SPY", "cash_at_risk":.5}
+)
+# trader = Trader()
+# trader.add_strategy(strategy)
+# trader.run_all()
